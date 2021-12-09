07e0ca9e24e69658c07768065a2aa852ef8eb56e
kalunge <muthomititus+github@gmail.com>
2021-12-09 11:14:07 +0100

    Add validations to Post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :Name, presence: { message: 'You must fill in your name' }
-  validates_numericality_of :PostsCounter, only_integer: true, greater_than_or_equal: 
+  validates_numericality_of :PostsCounter, only_integer: true, greater_than_or_equal: 0
 
   has_many :posts
   has_many :likes
