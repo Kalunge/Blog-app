6c57d0f379500e3e07dc47723ae7af9bb6e86da5
kalunge <muthomititus+github@gmail.com>
2021-12-13 21:29:10 +0100

    Add confirmation on login

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   def self.update_comment_counter(post_id)
-    Post.find_by(id: post_id).update(comments_counter: Comment.where(post_id: post_id).count)
+    Post.find_by(id: post_id).update(CommentsCounter: Comment.where(post_id: post_id).count)
   end
 end
